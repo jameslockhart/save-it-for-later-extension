eeec5276174d025f1db4e270b8c54540c4825059
James - The Original Master Branch <james@ilockhart.com>
2017-02-03 17:45:28 -0500

    finished firebase
    extension now sends link up to users profile on db

--- saveit.js
+++ saveit.js
@@ -13,21 +13,62 @@ $(document).ready(function() {
 		var email = $('#email').val();
 		var password = $('#password').val();
 
-		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-		  if (error.message) {
-			  $("#errorBox").html(error.message);
-			  $("#errorBox").show();
-			  setTimeout(function() {
-				$("#errorBox").fadeOut();
-			  }, 2000);	
-			} else {
-			  $("#successBox").html("Login successful!");
-			  $("#successBox").show();
-			  setTimeout(function() {
-				window.close();
-			  }, 2000);	
-			}
-		});
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(function(user) {
+				$("#successBox").html("Login successful!");
+			    $("#successBox").show();
+			    setTimeout(function() {
+					window.close();
+			    }, 2000);	
+			})
+			.catch(function(error) {
+			  if (error.message) {
+				  $("#errorBox").html(error.message);
+				  $("#errorBox").show();
+				  setTimeout(function() {
+					$("#errorBox").fadeOut();
+				  }, 2000);	
+				}
+			});
 	});
-	
+
+	// if we are logged in then send the page link to firebase
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			// hide the login peices
+			$('#email').hide();
+			$('#password').hide();
+			$('#login').hide();
+
+			function saveToDb(tab) {
+				var image = tab.favIconUrl
+				var postData = {
+				    image: image,
+				    read: false,
+				    title: tab.title,
+				    savedAt: new Date().toDateString(),
+				    url: tab.url
+				};
+
+				// Get a key for a new Post.
+				var newPostKey = firebase.database().ref().child('saves').child(user.uid).push().key;
+
+				// Write the new post's data simultaneously in the posts list and the user's post list.
+				var updates = {};
+				updates['/saves/' + user.uid + '/' + newPostKey] = postData;
+
+				firebase.database().ref().update(updates);
+
+				$("#successBox").html("Save successful!");
+			    $("#successBox").show();
+			    setTimeout(function() {
+					window.close();
+			    }, 1500);
+			}
+
+		    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+			    saveToDb(tabs[0]);
+			});
+		}
+	});
 });
